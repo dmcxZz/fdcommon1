06686c6272400332bf8d8ac0239da59cd2a60e7d
dmcxZz <zh_zhanghui@icloud.com>
2022-03-25 14:09:59 +0800

    add framework

---
+++ fdcommon1/Classes/FSDP.framework/Headers/FSDP.h
@@ -0,0 +1,38 @@
+//
+//  FSDP.h
+//  FSDP
+//
+//  Created by wangqiuli on 2020/6/19.
+//  Copyright © 2020 wangqiuli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FSDP.
+FOUNDATION_EXPORT double FSDPVersionNumber;
+
+//! Project version string for FSDP.
+FOUNDATION_EXPORT const unsigned char FSDPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FSDP/PublicHeader.h>
+
+#import <FSDP/LoggerUtil.h>
+#import <FSDP/ValidateUtil.h>
+#import <FSDP/NonUtil.h>
+#import <FSDP/QRCodeUtil.h>
+#import <FSDP/ImageUtil.h>
+#import <FSDP/MD5Util.h>
+#import <FSDP/ColorUtil.h>
+#import <FSDP/JsonUtil.h>
+#import <FSDP/AESUtil.h>
+#import <FSDP/DateUtil.h>
+#import <FSDP/HttpClientUtil.h>
+#import <FSDP/MobileMsgUtil.h>
+#import <FSDP/Base64Util.h>
+#import <FSDP/StringUtil.h>
+#import <FSDP/CacheUtil.h>
+#import <FSDP/ReturnValueDomain.h>
+#import <FSDP/PageDomain.h>
+#import <FSDP/BaseDomain.h>
+#import <FSDP/EventUtil.h>
+#import <FSDP/FDHeader.h>
